a6110bb069f122987f5be55f6ce932b77ad4d220
Anastasya Nosevich <nastianosevich@gmail.com>
2020-02-15 15:33:40 +0300

    feat: add func to setState because of it async

--- todo-yb/src/components/TodoListItem/index.js
+++ todo-yb/src/components/TodoListItem/index.js
@@ -5,34 +5,43 @@ export default class TodoListItem extends React.Component {
 
     state = {
         done: false,
+        important: false,
     }
 
     onLabelClick = () => {
-        this.setState({
-            done: !this.state.done,
+        this.setState(({ done }) => {
+            // put inside setState function because setState is async, and we are waiting for final state to toggle it
+            return {
+                done: !state.done,
+            }
         })
     }
 
-    render() {
-        const { label, important = false } = this.props;
-        const { done } = this.state;
-
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal',
+    onMarkInportanrt = () => {
+        if (!this.state.done) {
+            // put inside setState function because setState is async, and we are waiting for final state to toggle it
+            this.setState(({ important }) => {
+                return {
+                    important: !state.important,
+                }
+            })
         }
+    }
+
+    render() {
+        const { label } = this.props;
+        const { done, important } = this.state;
 
         return (
             <span className={done ? "todo-list-item done" : "todo-list-item"} >
-
                 <span
-                    className="todo-list-item-label"
-                    style={style}
+                    className={!done && important ? "todo-list-item-label important" : "todo-list-item-label"}
                     onClick={this.onLabelClick}>
                     {label}
                 </span>
                 <button type="button"
-                    className="btn btn-outline-success btn-sm float-right">
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onMarkInportanrt}>
                     <i className="fa fa-exclamation" />
                 </button>
                 <button type="button"
