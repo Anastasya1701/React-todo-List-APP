ead81c2c99b3bd894d33863d93a4563741f34130
Anastasya Nosevich <nastianosevich@gmail.com>
2020-02-16 21:37:57 +0300

    feat: add deploy version

---
+++ precache-manifest.399bf4175372c98b088d88f65719d836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56ff6f738679015ebf8a3a7cd04727f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4699c9e3bd84ece84ac6",
+    "url": "./static/css/main.42907c7d.chunk.css"
+  },
+  {
+    "revision": "391fe6a6de2214cd212f",
+    "url": "./static/js/2.50311cf8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.50311cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4699c9e3bd84ece84ac6",
+    "url": "./static/js/main.bce576ab.chunk.js"
+  },
+  {
+    "revision": "3e18bb2ee723f8804f2c",
+    "url": "./static/js/runtime-main.3e28f1ef.js"
+  }
+]);
